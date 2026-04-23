62dc86687666a0bfad58b3104e72ce9f73606c86
Naina-eng44 <chauhannaina2005@gmail.com>
2026-04-23 20:32:06 +0530

    Update Header component display

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 function Header({ name, role }) {
   return (
     <div className="text-center">
-      <img src="https://cdn-icons-png.flaticon.com/512/6997/6997662.png" alt-txt="profile image" className="profile-img"/>
+      <img src="https://cdn-icons-png.flaticon.com/512/6997/6997662.png" alt="profile" className="profile-img"/>
       <h3>{name}</h3>
       <p>{role}</p>
     </div>
